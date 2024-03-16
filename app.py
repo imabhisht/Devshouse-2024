9782d87997d40f36ce2571a9dfbc35f56f926ded
Abhisht Chouhan <abhishtchouhan@gmail.com>
2024-03-17 01:39:13 +0530

    Addded Features

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import os
 load_dotenv()
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
 user_id = os.getenv('USER_ID')
 # Connect to MongoDB
@@ -22,7 +24,8 @@ def create_project():
     try:
         db['projects'].insert_one({
             'name': new_data['name'],  
-            'user_id': user_id    
+            'user_id': user_id,
+            'description': new_data['description'],  
         })
         return jsonify({'message': 'Project created successfully'}), 200
     except Exception as e:
@@ -37,6 +40,34 @@ def get_projects():
         project['_id'] = str(project['_id'])
         data.append(project)
     return jsonify(data), 200
+
+
+@app.route('/projects/<id>', methods=['GET'])
+def get_project():
+    project = db['projects'].find_one({'_id': id})
+    project['_id'] = str(project['_id'])
+    return jsonify(project), 200
+
+@app.route('/projects/<id>', methods=['PUT'])
+def update_project(id):
+    new_data = request.json
+    try:
+        db['projects'].update_one({'_id': id}, {'$set': new_data})
+        return jsonify({'message': 'Project updated successfully'}), 200
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
+    
+
+## Update Workflow inside Project
+@app.route('/workflows/<id>', methods=['POST'])
+def create_workflow(id):
+    print(request.json)
+    new_data = request.json
+    try:
+        db['workflows'].update_one({'_id':id}, {"$set": new_data}, upsert=True)
+        return jsonify({'message': 'Workflow created successfully'}), 200
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
 
 
 
